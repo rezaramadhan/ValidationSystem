bb9122c85b0c86429d06701824005aea56670db8
Faza Thirafi <faza.thirafi@gmail.com>
2017-04-11 07:43:31 +0700

    front end loaded

--- form/views.py
+++ form/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
 
 # Create your views here.
 
 def validation_form(request):
-    return render(request, 'form/validation_form.html', {})
+     return render(request, 'form/validation_form.html', {})
+
