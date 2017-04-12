51bf55bda8da9b3fb8c155532eefb841491c555f
M. Reza Ramadhan <rezaramadhan.m@gmail.com>
2017-04-12 16:05:09 +0700

    Email validation work

--- email_validation/views.py
+++ email_validation/views.py
@@ -1,8 +1,15 @@
+from django.views.decorators.csrf import csrf_exempt
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from validate import validate_email
+import json
 
 # Create your views here.
-
+@csrf_exempt
 def validation_form(request):
-     return HttpResponse("testemail")
+    body_unicode = request.body.decode('utf-8')
+    body = json.loads(body_unicode)
+    email = body["email"]
+    result = validate_email(email)
+    response = "{ valid : '" + str(True) + "' }"
+    return HttpResponse(response)
